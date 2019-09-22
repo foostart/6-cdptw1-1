f04944c8c79077fa115e29144229a74e8641fb05
Duy Huynh <duyhuynh55110@gmail.com>
2019-09-23 02:25:13 +0700

    Upload code

--- 1180/1180-content.php
+++ 1180/1180-content.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <!-- Meta, title, CSS, favicons, etc. -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Module - Content e-commerce</title>
-
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" href="css/font-awesome.min.css">
-        <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
-
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/type-4.less', 'css/type-4.css');
-        ?>
-        <link rel="stylesheet" href="css/type-4.css">
-        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/canvasjs.min.js" type="text/javascript"></script>
-        <script src="js/chart-rain.js" type="text/javascript"></script>
-        <script src="js/collapse-close.js" type="text/javascript"></script>
-        <script src="js/jquery-ui.min.js" type="text/javascript"></script>
-        <script src="js/datetimepicker.js" type="text/javascript"></script>
-    </head>
-
-    <body>
-        <div class="type-4">
+<div class="type-1180">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
@@ -52,17 +19,16 @@
                                 <div class="col-md-9">
                                     <div id="chartRainContainer"></div>
                                     <div class="total-chart">
-                                        <div class="col-md-4 col-sm-4 col-xs-12">
                                             <span>Total Sessions</span>
                                             <h2>231,809</h2>
                                             <div id="chartCoalContainer2"></div>
                                         </div>
-                                        <div class="col-md-4 col-sm-4 col-xs-12">
+                                        <div class="col-chart col-md-4 col-sm-4 col-xs-12">
                                             <span>Total Revenue</span>
                                             <h2>231,809</h2>
                                             <div id="chartCoalContainer3"></div>
                                         </div>
-                                        <div class="col-md-4 col-sm-4 col-xs-12">
+                                        <div class="col-chart col-md-4 col-sm-4 col-xs-12">
                                             <span>Total Sessions</span>
                                             <h2>231,809</h2>
                                             <div id="chartCoalContainer4"></div>
@@ -192,5 +158,3 @@
 
             </div>
         </div>
-    </body>
-</html>
